364a4f27fa920a46727f97ef7c250970582afc28
voobscout <voobscout@archlinux.info>
2017-03-19 15:58:26 +0100

    ...

--- pushcfg.rb
+++ pushcfg.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'cocaine'
+require 'pathname'
+
 class Pushcfg < Sinatra::Base
   WORK_DIR = Pathname.new '/opt/bootkube'
 
@@ -18,35 +21,52 @@ class Pushcfg < Sinatra::Base
     respond
   end
 
+  private
   def respond
     begin
-      setvar
-      install_cfg
-      send(strip(@method).to_sym)
+      setvars
+      run_cmd
     rescue
       puts 'could not install kubecfg to ' + @ip
     end
   end
 
-  def install_cfg
-    `scp -oStrictHostKeyChecking=no -r #{@src_files.expand_path.to_s} core@#{@ip}:/home/core`
+  def setvars
+    @ip = @env['REMOTE_ADDR']
+    @method = strip(@env['REQUEST_PATH'])
+
+    ssh_opt = '-oStrictHostKeyChecking=no'
+    @scp = Cocaine::CommandLine.new("scp", ":opt -r :src :dst")
+    @scp_command = {
+                   opt: ssh_opt,
+                   src: @src_files.expand_path.to_s,
+                   dst: ('core@' + @ip + ':/home/core')
+                   }
+
+    @ssh = Cocaine::CommandLine.new("ssh", ":opt :dst :cmd")
+    @ssh_command = {
+                   opt: ssh_opt,
+                   dst: ('core@' + @ip)
+                   }
+
+    volumes = %w(/home/core/.ssh/id_rsa:/root/.ssh/id_rsa:ro /home/core/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub:ro).join(' -v ')
+    @docker = Cocaine::CommandLine.new("/usr/bin/docker", "-d --rm -ti -v #{volumes} ruby :cmd")
   end
 
-  def controller
-    `ssh -oStrictHostKeyChecking=no core@#{@ip} 'sudo mkdir -p /opt/bootkube && sudo mv /home/core/assets /opt/bootkube/assets && sudo systemctl start bootkube'`
+  def run_cmd
+    @docker.run(cmd: @scp.command(@scp_command))
+    @docker.run(cmd: @ssh.command(@ssh_command.merge(cmd: cmd[@method.to_sym])))
+    # puts @docker.command(cmd: @scp.command(@scp_command))
+    # puts @docker.command(cmd: @ssh.command(@ssh_command.merge(cmd: cmd[@method.to_sym])))
   end
 
-  def node
-    `ssh -oStrictHostKeyChecking=no core@#{@ip} 'sudo mkdir -p /etc/kubernetes && sudo mv /home/core/kubeconfig /etc/kubernetes/kubeconfig'`
+  def cmd
+    {controller: 'sudo mkdir -p /opt/bootkube && sudo mv /home/core/assets /opt/bootkube/assets && sudo systemctl start bootkube',
+     node: 'sudo mkdir -p /etc/kubernetes && sudo mv /home/core/kubeconfig /etc/kubernetes/kubeconfig'}
   end
 
   def strip slash
     slash.split('/').last
-  end
-
-  def setvar
-    @ip = @env['REMOTE_ADDR']
-    @method = @env['REQUEST_PATH']
   end
 
   run!
